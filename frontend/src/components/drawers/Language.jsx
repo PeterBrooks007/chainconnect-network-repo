a588b8ced8d19429a75d009408316736b8ce32f0
Peter Brooks <digitalyinvestassets@gmail.com>
2026-01-17 17:20:03 +0100

    my first commit

---
+++ frontend/src/components/drawers/Language.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect } from "react";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
+import { ArrowLeft, Moon, Sun, X } from "@phosphor-icons/react";
+import { tokens } from "../../theme";
+import EditProfile from "../EditProfile";
+import SecuritySystem from "../../pages/dashboard/dashboardComponents/SecuritySystem";
+import YandexTranslateWidget from "../YandexTranslateWidget";
+
+const Language = ({
+  open,
+  handleClose,
+  handleOpen,
+  profile,
+  setProfile,
+}) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      sx={{
+        "& .MuiDrawer-paper": {
+          width: {xs: "100%", md: "100%"}, 
+        },
+        zIndex: 20000
+      }}
+      keepMounted
+      
+    >
+      <Box
+        backgroundColor={colors.dashboardforeground[100]}
+        width={"100%"}
+        height={"100%"}
+        overflow={"auto"}
+      >
+       <AppBar
+          position="sticky"
+          sx={{
+            backgroundColor: `${
+              theme.palette.mode === "light"
+                ? "lightgrey"
+                : colors.dashboardbackground[100]
+            }`,
+            top: 0,
+            height: "56px"
+          }}
+          color="grey"
+        >
+          <Toolbar variant="dense" sx={{minHeight: "56px"}}>
+            <IconButton
+              edge="start"
+              
+              aria-label="menu"
+              sx={{ mr: 2, backgroundColor: "grey" }}
+              onClick={handleClose}
+              size="small"
+             
+            >
+              <ArrowLeft size={26} />
+            </IconButton>
+            <Typography variant="body1" color="inherit" component="div">
+              Select Language
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Box p={3}>
+          <YandexTranslateWidget />
+        </Box>
+
+      </Box>
+    </Drawer>
+  );
+};
+
+export default Language;
